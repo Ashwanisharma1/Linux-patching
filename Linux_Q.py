85938900a6cb20f92e30091126206aeaf4d95c88
ashwanisharma1 <ashwanirawat1991@gmail.com>
2026-02-14 08:42:41 +0530

    update

---
+++ Linux_Q.py
@@ -0,0 +1,17 @@
+# how to scan a newly added disk in linux in physical and vm 
+# how to revert kernel patch, 
+# swap and increase this
+# acls and user management 
+# how to patch, local repo, set a light and what is external setup.
+# how to disable a particual repo
+# how to disble without editing the file at command line
+# how to update only security patches
+# how to rollback in case of issue after server patch
+# if you are already in the latest kernel will undo of the patch history work?
+# how to extend the fs in physical as well as vm 
+# how to swap online without discturbing existing setup
+# after reboot nfs not connecting how to toubleshoot
+# how to refresh nfs service without affecting other file systems
+# cifs file system
+# how to perform the OS patching
+# 
